6eba47480e4738a518741ba08e976af3bfdb7bb5
Midnwave <cameron.crenshaw.cameron@gmail.com>
2026-01-22 14:15:39 -0600

    Remove unnecessary files and update gitignore

--- grimac-bukkit-2.3.72.jar.src (1)/ac/grim/grimac/manager/init/Initable.java
+++
@@ -1,9 +0,0 @@
-package ac.grim.grimac.manager.init;
-
-public interface Initable {}
-
-
-/* Location:              C:\Users\Cameron Crenshaw\Downloads\grimac-bukkit-2.3.72.jar!\ac\grim\grimac\manager\init\Initable.class
- * Java compiler version: 17 (61.0)
- * JD-Core Version:       1.1.3
- */
--- grimac-bukkit-2.3.72.jar.src (1)/ac/grim/grimac/shaded/fastutil/objects/AbstractObjectSpliterator.java
+++
@@ -1,9 +0,0 @@
-package ac.grim.grimac.shaded.fastutil.objects;
-
-public abstract class AbstractObjectSpliterator<K> implements ObjectSpliterator<K> {}
-
-
-/* Location:              C:\Users\Cameron Crenshaw\Downloads\grimac-bukkit-2.3.72.jar!\ac\grim\grimac\shaded\fastutil\objects\AbstractObjectSpliterator.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
